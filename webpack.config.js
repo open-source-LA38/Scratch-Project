b1c1792ab615fd44511a168e2cac4d701209fa0d
Joonyoung <impervious7@gmail.com>
2020-09-11 23:25:58 -0700

    implementing webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  mode: 'process.env.NODE_ENV', 
+  entry: './client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+module: {
+  rules: [
+    {
+     test: /\.jsx?/,
+     use:{
+       loader: "babel-loader",
+       options: {
+         presets: ['@babel/preset-env', '@babel/preset-react']
+       } 
+     }, 
+     exclude: "node_modules"      
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }
+  ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
