6be9eacfd0ba7e479749d2d3a3bd40eb37c62c3f
VNguyenCode <vnguyencode@gmail.com>
2020-09-13 09:18:18 -0700

    Auth Done

--- client/components/login/Login.jsx
+++ client/components/login/Login.jsx
@@ -1,31 +1,37 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import './loginstyle.css';
 
 const Login = () => {
   return (
     <React.Fragment>
-      <div className="logincontainer">
-        <h1>Welcome!</h1>
-        <form method="POST" action="/api/login">
-          <div className="input">
-            <p>Username</p>
-            <div>
-              <input name="username" type="text"></input>
-            </div>
-            <p>Password</p>
-            <div>
-              <input name="password" type="text"></input>
-            </div>
-            <input type="submit" value="login"></input>
+      <div className="flex-container">
+        <div className="flex-item">
+          <div className="logincontainer">
+            <h1>Welcome!</h1>
+            <form method="POST" action="/api/login">
+              <div className="input">
+                <p>Username</p>
+                <div>
+                  <input name="username" type="text"></input>
+                </div>
+                <p>Password</p>
+                <div>
+                  <input name="password" type="text"></input>
+                </div>
+                <input type="submit" value="login"></input>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
 
-      <div className="registerbutton">
-        <Link to="/signup" className="btn btn-primary">
-          Need to sign up?
-        </Link>
+          <div className="registerbutton">
+            <p>Don't have an account yet? </p>
+            <Link to="/signup" className="btn btn-primary">
+              Need to sign up?
+            </Link>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,42 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  mode: 'development', 
+  mode: 'development',
   entry: './client/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
-    publicPath: '/'
+    publicPath: '/',
   },
-module: {
-  rules: [
-    {
-     test: /\.jsx?/,
-     use:{
-       loader: "babel-loader",
-       options: {
-         presets: ['@babel/preset-env', '@babel/preset-react']
-       } 
-     }, 
-     exclude: /node_modules/     
-    },
-    {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    }
-  ]
- },
- resolve: {
-     //enable importing js jsx files without specifying their extension
-     extensions: [".js", ".jsx"],
- },
- devServer: {
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    //enable importing js jsx files without specifying their extension
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
     historyApiFallback: true,
-    contentBase: path.resolve(__dirname, "./client"),
+    contentBase: path.resolve(__dirname, './client'),
     port: 8080,
     proxy: {
-        "/api": "http://localhost:3000"
+      '/api': 'http://localhost:3000',
     },
-    publicPath: "/build/"
- }
-}
+    publicPath: '/build/',
+  },
+};
