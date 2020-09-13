48011579f19985e8e5d7a474cc2bb22af7251761
Lucy Chi <lucycchi@gmail.com>
2020-09-13 09:00:51 -0700

    started on maincontroller storeUrl

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ google.account for Elephant SQL (OAth with google account)
 username: pinkfairyarmadillo38
 password: codesmithLA
 birthday: Jan. 1, 2000
+
+node-cron : to schedule tasks
+
+https://www.npmjs.com/package/node-cron
+
+https://www.digitalocean.com/community/tutorials/nodejs-cron-jobs-by-examples
+
+https://scotch.io/tutorials/nodejs-cron-jobs-by-examples
--- server/controller/maincontroller.js
+++ server/controller/maincontroller.js
@@ -1,11 +1,24 @@
-
 const path = require("path");
 const fs = require("fs");
 const maincontroller = {};
-const db = require('../db/databaseIndex.js')
+const db = require("../db/databaseIndex.js");
 
+/*Twilio express sms docs
+https://www.twilio.com/docs/sms/tutorials/how-to-send-sms-messages-node-js
+*/
+//node-cron/scheduler to schedule tasks
 
+/*Timestamp for psql
+https://www.sqlservertutorial.net/sql-server-date-functions/sql-server-current_time-function/
+*/
 
+/*getstatus - to be used inside middleware and node-cron
+input is URL or array of URLs to ping
+pings URL, saves to database
+output is received status code
+saveToDb - include our query to save to database, parameter will be status returned from api ping
+https://www.restapitutorial.com/httpstatuscodes.html
+*/
 
 /*3) user adds in URL that they want to track
 api= /addURL
@@ -17,30 +30,56 @@ twillio API for text messages*/
 
 //storeUrl - store URL in database, store default interval in database
 maincontroller.storeUrl = (req, res, next) => {
-//fetch- 
+  //receive Url from req.body, interval defaulted to 1 hour
+  const Url = req.body;
+  const UrlforQuery = [Url];
+  let status;
+  let UrlId;
+  //https://www.postgresql.org/docs/9.0/dml-insert.html
+  const selectUrlIdQuery = "SELECT url.url_id FROM url WHERE url.url = $1";
+  db.query(selectUrlIdQuery, UrlforQuery)
+    .then((data) => {
+      UrlId = data;
+    })
+    .catch((error) =>
+      next({
+        log:
+          "Express error handler caught error in maincontroller.storeUrl in db query selectUrlQuery",
+        status: 400,
+        message: { err: error },
+      })
+    );
+  //database will insert timestamp into status table when new statu is inserted
+  const addStatusQuery = "INSERT INTO status (url_id, status) VALUES ()";
+  //fetch- !) ping Url, 2)if ping returns 200 message- we know Url is valid, 3)store Url in database, 4)invoke scheduler, 5)send status code to client (res.locals)
+  //https://blog.logrocket.com/axios-or-fetch-api/#:~:text=To%20send%20data%2C%20fetch(),set%20in%20the%20options%20object
+  fetch(Url)
+    .then((res) => {
+      status = res.status;
+      //insert to Url table
+      //insert to status table
+    })
+    .catch((err) => {});
+
+  //invoke getstatus to ping url, receive status code
 
   next();
-}
+};
 
 //pingUrlInterval - A- retrieve URL and interval from database, B-set timer to ping URL, C-send message to twilio if status is not 200, D- save status code and time in database
 maincontroller.pingUrlInterval = (req, res, next) => {
   next();
-}
+};
 
 //updateInterval - update interval in database
 maincontroller.updateInterval = (req, res, next) => {
   next();
-}
+};
 
 //checkUrlNow - A- ping URL, B- save status code and time in database - use URL to tell postgres where to store status code and time stamp, C-send to client URL status code in res.locals
 maincontroller.checkUrlNow = (req, res, next) => {
   next();
-}
-
-
-
-
-
+};
 
 /*6) - data pull[https://mdbootstrap.com/docs/react/advanced/charts/](https://mdbootstrap.com/docs/react/advanced/charts/)
 get historical data from database , will be default time (we will test to determine later)
@@ -50,9 +89,8 @@ res.locals = will send back 2 arrays
 A)all the times URL was pinged
 B)all the status codes */
 //getData 5 -query the database for times and status code for url given in req.body, then save to res.locals and send back a res contiaing res.locals
-maincontroller.getData = (req,res,next) => {
+maincontroller.getData = (req, res, next) => {
   next();
-}
-
+};
 
 module.exports = maincontroller;
